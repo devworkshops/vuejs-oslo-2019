e0bf228702e6cecea14193cbbea0cdbbe7ef2e68
Thiago Passos <thiago@passos.com.au>
2019-06-18 00:19:30 +1000

    updating navbar

--- northwind-traders/src/components/NavBar.vue
+++ northwind-traders/src/components/NavBar.vue
@@ -1,27 +1,19 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="#">Northwind Traders</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarCollapse">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">Northwind Traders</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
-          <router-link to="/" tag="li" :exact="true" class="nav-item" active-class="active">
-            <a class="nav-link">Home</a>
-          </router-link>
-          <router-link to="/suppliers" tag="li" class="nav-item" active-class="active">
-            <a class="nav-link">Suppliers</a>
-          </router-link>
-        </ul>
-      </div>
-    </div>
-  </nav>
+    <b-collapse id="nav-collapse" is-nav>
+      <ul class="navbar-nav mr-auto">
+        <router-link to="/" tag="li" :exact="true" class="nav-item" active-class="active">
+          <a class="nav-link">Home</a>
+        </router-link>
+        <router-link to="/suppliers" tag="li" class="nav-item" active-class="active">
+          <a class="nav-link">Suppliers</a>
+        </router-link>
+      </ul>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
